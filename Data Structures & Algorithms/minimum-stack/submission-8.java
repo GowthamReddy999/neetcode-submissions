817a78fb51728441978300f6a20f4e8c5175b623
GowthamReddy999 <gowthamavula0906@gmail.com>
2026-07-07 11:32:10 +0530

    Add: minimum-stack - submission-8

---
+++ Data Structures & Algorithms/minimum-stack/submission-8.java
@@ -0,0 +1,55 @@
+class MinStack {
+
+    Stack<Integer> s1;
+    Stack<Integer> ms;
+    public MinStack() {
+        s1 = new Stack<>();
+        ms = new Stack<>();
+    }
+    
+    public void push(int val) {
+        s1.push(val);
+        if(ms.isEmpty()){
+            ms.push(val);
+        }
+        else{
+            if(val<=ms.peek()){
+                ms.push(val);
+            }
+            
+        }
+
+    }
+    
+    public void pop() {
+
+       
+        int popval = s1.pop();
+        if(ms.peek()==popval){
+            
+            
+            ms.pop();
+
+        }
+        
+
+    }
+    
+    public int top() {
+        if(s1.isEmpty()){
+            return -1;
+        }
+        else{
+            return s1.peek();
+        }
+    }
+    
+    public int getMin() {
+        if(ms.isEmpty()){
+            return -1;
+        }
+        else{
+            return ms.peek();
+        }
+    }
+}
