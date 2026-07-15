70fbe3b7835aee1e4ae4baeecc3da6a142ebc7ce
GowthamReddy999 <gowthamavula0906@gmail.com>
2026-07-15 21:38:43 +0530

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums){
+       ArrayList<Integer> users = new ArrayList<>();
+
+       for(int n : nums){
+
+        if(users.contains(n)){
+            return true;
+        }
+        users.add(n);
+       }
+       return false;
+    }
+}
