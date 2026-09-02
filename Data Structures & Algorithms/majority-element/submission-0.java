8ef4af989a006e069fb5fd446a926da7296383e0
GowthamReddy999 <gowthamavula0906@gmail.com>
2026-09-02 20:06:52 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        
+           int candiate = 0;
+           int count = 0;
+        for(int num : nums){
+
+            if(count == 0){
+                candiate = num;
+            }
+
+            if(num == candiate){
+                count++;
+            }
+            else{
+                count--;
+            }
+
+        }
+        return candiate;
+    }
+}
